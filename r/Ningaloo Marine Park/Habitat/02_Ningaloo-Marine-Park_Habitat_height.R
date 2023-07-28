6aa419678df11049a70ef3c61503581acde1e54c
BrookeGibbons <21301367@student.uwa.edu.au>
2023-07-28 11:58:52 +0800

    tidy habitat height script

--- r/Ningaloo Marine Park/Habitat/02_Ningaloo-Marine-Park_Habitat_height.R
+++ r/Ningaloo Marine Park/Habitat/02_Ningaloo-Marine-Park_Habitat_height.R
@@ -17,10 +17,28 @@ library(sf)
 # TODO turn this into a package.
 source("r/00_Functions-for-API (do not run).R")
 
+# Set synthesis
+synthesis <- "Ningaloo"
 synthesis_id <- 17 # For Ningaloo Fish + Habitat Syntheses
 
 # API call for Metadata ----
 metadata <- ga.api.metadata(synthesis_id)
 
 # API call for Habitat height ----
-habitat_height <- ga.api.habitat.height(synthesis_id)
+habitat_height <- ga.api.habitat.height(synthesis_id) %>%
+  dplyr::select(sample, level_2, length, number) %>%
+  dplyr::filter(!level_2 %in% "Unscorable") %>%
+  left_join(metadata)
+
+# Save out data
+write.csv(habitat_height, paste0("data/tidy/", synthesis,"_habitat-height.csv"), row.names = F)
+
+# Plot data
+ggplot(data = habitat_height, aes(x = level_2, y = length)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_point(alpha = 0.2, position = position_jitter(w = 0.1, h = 0)) +
+  labs(x = NULL, y = "Length (mm)") +
+  theme_classic()
+
+ggsave(filename = paste0("plots/", paste(synthesis,"habitat-height", "boxplot.png", sep = "_")),
+       plot = last_plot(), width = 8, height = 6, units = "in", dpi = 300)
